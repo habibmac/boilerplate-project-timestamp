d8d84c9d593c04f75f6f091bd4913eb70e0a7d4d
habibmac <mcrury@gmail.com>
2023-12-07 15:40:19 +0700

    fix: vercel conf

--- index.js
+++ index.js
@@ -47,3 +47,5 @@ app.get("/api/:date?", function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
     console.log("Your app is listening on port " + listener.address().port);
 });
+
+module.exports = app;
